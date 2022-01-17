ccd88cdab8638a960401e6eec320640c7f4cb00c
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2022-01-17 03:07:38 +0000

    tests

---
+++ main.js
@@ -0,0 +1,47 @@
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
+
+function getData(type, cb){
+    var xhr = new XMLHttpRequest();
+
+
+xhr.open("GET", baseURL + type + "/");
+xhr.send();
+
+
+xhr.onreadystatechange = function(){
+    console.log(this,this.readyState);
+    if(this.readyState == 4 && this.status == 200){
+        // document.getElementById("data").innerHTML = this.responseText;
+       cb(JSON.parse(this.responseText));
+    }
+};
+}
+
+function getTableHeaders(obj){
+    var tableHeaders = [];
+    Object.keys(obj).forEach(function(key){
+        tableHeaders.push(`<td>${key}</td>`)
+    });
+    return `<tr>${tableHeaders}</tr>`;
+}
+
+function writeToDocument(type){ 
+    var tableRows = [];
+    var el = document.getElementById("data");
+    el.innerHTML = "";
+    getData(type, function(data){
+        data = data.results;
+        var tableHeaders = getTableHeaders(data[0]);
+        data.forEach(function(item){
+           var dataRow = [];
+           Object.keys(item).forEach(function(key){
+               var rowData = item[key].toString();
+               var truncatedData = rowData.substring(0,15);
+               dataRow.push(`<td>${truncatedData}</td>`);
+           });
+           tableRows.push(`<tr>${dataRow}</td>`);
+            // el.innerHTML += "<p>" + item.name + "</p>";
+        });
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+    });
+}
